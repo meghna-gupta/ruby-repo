9838a8c78c8370cab641313a3256b54bbee855df
meghna-gupta <meghnaguptajaipur@gmail.com>
2022-03-29 16:04:58 +0530

    My first todo program example s

---
+++ todos.rb
@@ -0,0 +1,12 @@
+# ask for a list of todos
+# and print them back to us
+
+puts "What is your next todo?"
+next_todo = gets
+
+# puts "So, this is your next todo:"
+# puts next_todo
+
+puts "\n\n\n"       # Carriage return
+
+puts "[ ] #{next_todo}"    # String Interpolation
